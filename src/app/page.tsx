7dbbb0706dc20ff308fb76c6ed5d918f7783b95c
gsdevmac <gsdevbali@gmail.com>
2025-09-29 12:08:27 +0800

    m1 DB ON

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>XHRIS is ON</h1>
+      <p>DB on</p>
     </div>
   );
 }
