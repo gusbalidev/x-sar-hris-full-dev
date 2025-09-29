a20ae7cdab410bd60809b7886ab3c6eaf3a7b522
gsdevmac <gsdevbali@gmail.com>
2025-09-29 12:28:39 +0800

    m1 better-auth start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>XHRIS is ON</h1>
       <p>DB on</p>
+      <p>Better-Auth on</p>
     </div>
   );
 }
